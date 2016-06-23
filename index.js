3196bb14d05c97c667e76ce4f43cb1f59b9d0790
mmalpani <megha.malpani@gmail.com>
2016-06-23 14:39:30 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+function sensor(name, deps) {
+  console.log('This is where Telemetry code would execute in the node process.');
+
+  var statusdata = {};
+
+  deps.globalEventLoop.on( 'physicalInterface.status', function(data){
+    for (var i in data) {
+      if (i === 'cmd'){
+        //filter out ping command echos
+        if (data[i].indexOf('ping')>=0) continue;
+      }
+      statusdata[i] = data[i];
+    }
+  });
+
+
+  setInterval(function () {
+    deps.cockpit.emit('plugin.telemetry.logData', statusdata);
+  }, 1000);
+
+
+
+}
+module.exports = function (name, deps) {
+  return new sensor(name,deps);
+};
