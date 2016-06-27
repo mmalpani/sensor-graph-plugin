b516236cbb3cbcbb7cb79459dff91d2957ceb89b
mmalpani <megha.malpani@gmail.com>
2016-06-27 11:58:33 -0700

    First commit

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
+//node side - what goes down to the ROV 
 function sensor(name, deps) {
   console.log('This is where Telemetry code would execute in the node process.');
-
-  var statusdata = {};
-
-  deps.globalEventLoop.on( 'physicalInterface.status', function(data){
-    for (var i in data) {
-      if (i === 'cmd'){
-        //filter out ping command echos
-        if (data[i].indexOf('ping')>=0) continue;
-      }
-      statusdata[i] = data[i];
-    }
-  });
-
-
-  setInterval(function () {
-    deps.cockpit.emit('plugin.telemetry.logData', statusdata);
-  }, 1000);
-
-
 
 }
 module.exports = function (name, deps) {
